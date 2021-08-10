daf70987f16317324ce8bf09eb327d5b8e1fce91
egortcevasofia <egortcevasofia@mail.ru>
2021-08-10 09:33:08 +0300

    Удалена аннотация в классе LessonControllerTest

--- src/test/java/com/example/demo/controller/LessonControllerTest.java
+++ src/test/java/com/example/demo/controller/LessonControllerTest.java
@@ -24,7 +24,6 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
-@AutoConfigureMockMvc
 @WebMvcTest(LessonController.class)
 @ContextHierarchy({
         @ContextConfiguration(classes = MappersTestConfig.class)
