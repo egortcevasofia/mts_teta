fe9410e9db060e98183927a61f9106303c092f32
egortcevasofia <egortcevasofia@mail.ru>
2021-08-18 17:07:32 +0300

    Поправлены представления по исключениям

--- src/main/java/com/example/demo/controller/ControllerAdviser.java
+++ src/main/java/com/example/demo/controller/ControllerAdviser.java
@@ -12,28 +12,32 @@ public class ControllerAdviser {
 
     @ExceptionHandler(NotFoundException.class)
     public ModelAndView notFoundExceptionHandler(NotFoundException ex) {
-        ModelAndView modelAndView = new ModelAndView("not_found");
+        ModelAndView modelAndView = new ModelAndView("exception_view");
+        modelAndView.addObject("message", "Пользователь или курс не найдены");
         modelAndView.setStatus(HttpStatus.NOT_FOUND);
         return modelAndView;
     }
 
     @ExceptionHandler(NotPossibleDeleteException.class)
     public ModelAndView notPossibleDeleteExceptionHandler(NotPossibleDeleteException ex) {
-        ModelAndView modelAndView = new ModelAndView("not_possible_delete");
+        ModelAndView modelAndView = new ModelAndView("exception_view");
+        modelAndView.addObject("message", "Невозможно удалить пользователя, так как на него назначены курсы");
         modelAndView.setStatus(HttpStatus.BAD_REQUEST);
         return modelAndView;
     }
 
     @ExceptionHandler(UserAlreadyExistsException.class)
     public ModelAndView notPossibleDeleteExceptionHandler(UserAlreadyExistsException ex) {
-        ModelAndView modelAndView = new ModelAndView("user_already_exists");
+        ModelAndView modelAndView = new ModelAndView("exception_view");
+        modelAndView.addObject("message", "Такой логин уже создан, придумайте другой");
         modelAndView.setStatus(HttpStatus.BAD_REQUEST);
         return modelAndView;
     }
 
     @ExceptionHandler(InternalServerError.class)
     public ModelAndView InternalServerErrorExceptionHandler(InternalServerError ex) {
-        ModelAndView modelAndView = new ModelAndView("user_already_exists");
+        ModelAndView modelAndView = new ModelAndView("exception_view");
+        modelAndView.addObject("message", "Произошла ошибка на сервере, попробуйте еще раз");
         modelAndView.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
         return modelAndView;
     }
